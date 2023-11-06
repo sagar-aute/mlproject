c10e858a0387e0f93caa25bc85180ba69ef10fe6
Sagar Aute <sagaraute090@gmail.com>
2023-11-07 00:59:01 +0530

    MLFLOWr

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## End to End Data Science Project
+
+MLFLOW_TRACKING_URI=https://dagshub.com/sagar-aute/mlproject.mlflow \
+MLFLOW_TRACKING_USERNAME=sagar-aute \
+MLFLOW_TRACKING_PASSWORD=5f1ad79cea26665193ebda2495aae3e1ad162fa6 \
+python script.py
--- src/mlproject/components/model_trainer.py
+++ src/mlproject/components/model_trainer.py
@@ -30,6 +30,12 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
 
+    def eval_metrics(self, actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
+
     def initiate_model_trainer(self,train_array,test_array):
         try:
             logging.info("Split training and test input data")
@@ -100,6 +106,37 @@ class ModelTrainer:
 
             print("This is the best model:")
             print(best_model_name)
+
+            model_names = list(params.keys())
+
+            actual_model=""
+
+            for model in model_names:
+                if best_model_name == model:
+                    actual_model = actual_model + model
+
+            best_params = params[actual_model]
+
+            mlflow.set_registry_uri("https://dagshub.com/sagar-aute/mlproject.mlflow")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+            #mlflow
+            with mlflow.start_run():
+                predicted_qualities = best_model.predict(X_test)
+                
+                (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+                
+                mlflow.log_params(best_params)
+                
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
+
+                if tracking_url_type_store != "file":
+                    mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
+                else:
+                    mlflow.sklearn.log_model(best_model, "model")
+
 
             if best_model_score<0.6:
                 raise CustomException("No best model found")
